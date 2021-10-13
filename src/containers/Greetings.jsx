2d0a55a0f77e048b5b9c9002794caf7d8667899e
angusmacdonald <n10448888@qut.edu.au>
2021-10-14 02:02:23 +1000

    added download file name

--- src/containers/Greetings.jsx
+++ src/containers/Greetings.jsx
@@ -50,7 +50,7 @@ const Greetings = () => {
                           className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
                           color="default"
                           href={Pdf}
-                          download
+                          download="angus_macdonald.pdf"
                         >
                           <span className="btn-inner--icon mr-1">
                             <i className="fa fa-file" />
